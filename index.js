93bd13ef895ae5becb1cf98e277f9940387ed84a
Nivedh Ollakal <1ollakal@gmail.com>
2020-11-07 17:17:23 -0500

    Boilerplate Setup

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const ejsLayouts = require('express-ejs-layouts')
+
+//  setup ejs and ejs layouts
+app.set('view engine', 'ejs')
+app.use(ejsLayouts)
+
+// body parser middleware (this makes req.body work)
+app.use(express.urlencoded({extended: false}))
+
+// use controllers
+app.use('/auth', require('./controllers/auth.js'))
+
+app.get('/', (req, res)=>{
+    res.send('EXPRESS AUTH HOME ROUTE')
+})
+
+app.listen(8000, ()=>{
+    console.log('you\'re listening to the spooky sounds of port 8000')
+})
